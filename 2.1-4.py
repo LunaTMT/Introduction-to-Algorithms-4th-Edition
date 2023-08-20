0501e97a3c1ac04ae8222f5324a819e2fa5969fc
T Shredder <44672093+LunaTMT@users.noreply.github.com>
2023-08-20 16:14:56 +0100

    Update 2.1-4.py

--- 2.1-4.py
+++ 2.1-4.py
@@ -1,14 +1,12 @@
+# 2.1-4
 # Page 25
 # introduction to algorithms 4th ed
-# 2.1-4
+
 arr = [1, 2, 3, 4, 5]
 target = 5
 i = 0
 n = len(arr)
 while i < n-1 and arr[i] != target:
     i += 1
-    
-if arr[i] == target:
-    print(f"Index : {i}")
-else: 
-    print(None)
+
+print(f"{f'Index : {i}' if arr[i] == target else None}")
