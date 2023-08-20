a67d9161ff16a386b899c5e6cf96dae0587216a4
T Shredder <44672093+LunaTMT@users.noreply.github.com>
2023-08-20 12:05:12 +0100

    Update 1.2-2.py

--- 1.2-2.py
+++ 1.2-2.py
@@ -1,4 +1,4 @@
-from math import log, e
+from math import log2
 
 """
 suppose insertion sort uns at 8n^2 and,
@@ -7,12 +7,12 @@ For which values of n does insertion sort beat merge sort
 """
 
 def merge_sort(n):
-    return int(64*n * log(e) )
+    return int(64*n * log2(n))
 
 def insertion_sort(n):
     return 8 * (n ** 2)
 
-n = 0
+n = 2
 m = merge_sort(n)
 i = insertion_sort(n)
 
@@ -26,4 +26,16 @@ while not (i > m):
           Insertion > Merge : {i > m}
           """)
     n += 1
-print(f"Thus Insertion sort beats merge sort in this instance when n = {n}")
+print(f"Thus Insertion sort beats merge sort in this instance when n = {n-1}")
+
+"""
+Exercise 1.2-2
+We wish to determine for which values of n the inequality 
+8n^2 < 64n log2(n) holds. 
+
+This happens when n < 8 log2(n), or when n ≤ 43. 
+
+In other words,
+insertion sort runs faster when we’re sorting at most 43 items. Otherwise merge
+sort is faster.
+"""
