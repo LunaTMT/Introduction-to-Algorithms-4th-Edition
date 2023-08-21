0827b9bdad487a4cf85b9786b7de3399f8f9b588
T Shredder <44672093+LunaTMT@users.noreply.github.com>
2023-08-21 18:28:12 +0100

    Update 2.2-2.py

--- 2.2-2.py
+++ 2.2-2.py
@@ -1,9 +1,14 @@
-# 2.2-2
-# Page 33
-# Introduction to algorithms 4th ed
+"""
+ 2.2-2
+ Page 33
+ Introduction to algorithms 4th ed
 
-#worst case: n^2
-#best case: n^2
+ Worst case: n^2
+ Best case: n^2
+
+The following algorithm only needs to run for n-1 elements for by the time it 
+reaches the last element it is alreadt in the correct order. I.e. it is the largest element 
+"""
 
 arr = [5, 4, 6, 5, 2, 6]
 i = 0
@@ -19,5 +24,5 @@ for i in range(0, n-1):
             lowest = j
         j+=1 
     arr[i], arr[lowest] = arr[lowest], arr[i]
-    
+
 print(arr)
